c0ad4e0c814de7fd930f733defc1a16fe1a412b1
Mats Larsen <me@supergrecko.com>
2021-07-11 14:56:29 +0200

    build: re-export and bundle compiled code

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export type {
+  CompilerBase,
+  LibraryEntry,
+  Library,
+  ToolEntry,
+  Tool,
+  DefaultCompilerFields,
+  CompilerResponse
+} from './schema/compiler'
+export type {
+  Language,
+  DefaultLanguageFields,
+  LanguageResponse
+} from './schema/language'
+export { CompilerExplorer } from './http'
